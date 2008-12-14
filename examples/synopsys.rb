75b74c0a746c061898b9b6f1b75df1c661d85dae
Avdi Grimm <avdi@avdi.org>
2008-12-14 16:29:49 -0500

    Updated documentation

--- examples/synopsys.rb
+++ examples/synopsys.rb
@@ -2,4 +2,37 @@ require File.expand_path('../lib/clafamatt', File.dirname(__FILE__))
 
 module Shared
   include Clafamatt::Macros
+
+  class_family_accessor :foo
 end
+
+class Parent
+  include Shared
+
+end
+
+class Child < Parent
+  class_family_accessor :bar
+end
+
+Shared.foo                      # =>
+Parent.foo                      # =>
+Child.foo                       # =>
+
+Shared.foo = "klaatu"
+Shared.foo                      # =>
+Parent.foo                      # =>
+Child.foo                       # =>
+
+Parent.foo = "nikto"
+Shared.foo                      # =>
+Parent.foo                      # =>
+Child.foo                       # =>
+
+Child.foo = "barada"
+Parent.foo                      # =>
+Child.foo                       # =>
+
+Child.bar = "klaatu"
+Child.bar                       # =>
+Parent.bar                      # =>
